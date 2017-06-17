c5c646fed882a4f8bea9b646fd2de52484a77c66
zengkv <740832946@qq.com>
2017-06-17 14:55:12 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+/*global module*/
+
+var glob = require("glob");
+var path = require("path")
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+var fs = require('fs');
+if (fs.existsSync(".babelrc") == false) {
+    fs.writeFileSync(".babelrc", '{ "presets": ["es2015", "stage-3"] }')
+}
+
+var files = glob.sync('./src/*.js');
+var entry = {};
+
+files.forEach(function(f){
+   var name = /.*\/(\w+)\.js$/.exec(f)[1];//得到apps/question/index这样的文件名
+   entry[name] = f;
+});
+
+module.exports = {
+  entry: entry,
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules|web_modules/, loader: 'babel-loader' },
+      { test: /\.png$/, loader: 'url-loader?limit=5000' },
+      { test: /\.json$/, loader: 'json' }
+    ]
+  },
+  plugins: [
+    new BrowserSyncPlugin({
+      server: { baseDir: ['./public'] },
+      open: false
+    })
+  ]
+};
